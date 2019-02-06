3c7e1836b282b33bceda2de5c6057b5141f293c2
Vilem Jenis <vilemj@kentico.com>
2019-02-06 19:49:14 +0100

    Add comma to options declaration to match the implicit help option definition.

--- server/src/arguments.ts
+++ server/src/arguments.ts
@@ -8,10 +8,10 @@ type Arguments = {
 };
 
 const argParser = commander
-  .option('-w --watch', 'Indicate whether you want to recompile after client files change.', false)
-  .option('-j --inline-js', 'Indicate whether you want to inline JS into the served HTML.', false)
-  .option('-s --inline-styles', 'Indicate whether you want to inline styles in the served HTML.', false)
-  .option('-c --compile', 'Compile all custom modules into one HTML file to be then served as a static file.', false);
+  .option('-w, --watch', 'Indicate whether you want to recompile after client files change.', false)
+  .option('-j, --inline-js', 'Indicate whether you want to inline JS into the served HTML.', false)
+  .option('-s, --inline-styles', 'Indicate whether you want to inline styles in the served HTML.', false)
+  .option('-c, --compile', 'Compile all custom modules into one HTML file to be then served as a static file.', false);
 
 export const getArguments = (argv: Array<string>): Arguments => {
   const args = argParser.parse(argv);
