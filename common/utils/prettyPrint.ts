cfde57f8d88c50abafe918e4a043b58b35512d23
Vilem Jenis <vilemj@kentico.com>
2019-02-06 22:27:41 +0100

    Lint flaws out of the code.

--- common/utils/prettyPrint.ts
+++ common/utils/prettyPrint.ts
@@ -1,7 +1,7 @@
-export const prettyPrint = <T extends object>(arg: T): void => {
-  console.log(prettyString(arg));
-};
-
 export const prettyString = <T extends object>(arg: T): string => {
   return JSON.stringify(arg, null, 2);
+};
+
+export const prettyPrint = <T extends object>(arg: T): void => {
+  console.log(prettyString(arg));
 };
